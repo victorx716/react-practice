58ada7916b5611969b9d995822b7427bf5fdf62e
Victor Liu <vliu@ibm.com>
2019-09-10 13:36:50 -0500

    Implement sass styling on header and container divs

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -79,12 +79,14 @@ class IndecisionApp extends React.Component {
     return (
       <div>
         <Header  subtitle={subtitle} />
+        <div className = "container">
         <Action hasOptions={this.state.options.length > 0} handlePick = {this.handlePick} />
         <Options options={this.state.options} 
           handleDeleteOptions = {this.handleDeleteOptions}
           handleDeleteOption = {this.handleDeleteOption}
         />
         <AddOption handleAddOption = {this.handleAddOption}/>
+        </div>
         <OptionModal selectedOption={this.state.selectedOption} handleClearOption = {this.handleClearOption}/>
       </div>
     );
